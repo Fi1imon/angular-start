b9982a6f230a243b306fba20fdaedecd0df032ce
Fi1imon <angelo4ekfilippof@gmail.com>
2022-01-31 21:00:04 +0300

    dynamic classes with [ngClass] two eventspeacers in 1 object with NO zapyataya

--- src/app/sex/sex.component.ts
+++ src/app/sex/sex.component.ts
@@ -9,6 +9,15 @@ export class SexComponent implements OnInit {
 
   backTurnOn = false
 
+  buttTxt = 'Turn On'
+
+  buttChange (event: any) {
+    if (this.buttTxt === 'Turn On') {
+      this.buttTxt = 'Turn Off'
+    }
+    else this.buttTxt = 'Turn On'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
