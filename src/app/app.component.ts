2a6a7d511735e16053b66690807983ae52d6daa3
Fi1imon <angelo4ekfilippof@gmail.com>
2022-02-02 21:14:08 +0300

    pipe | Ended 'Основы'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arr = [1, 4, 8, 8]
-
-  dogs = [
-    {title: 'dog 1', Name: 'Popka', comments:[
-        {owner: 'Sasha', age: 24},
-        {owner: 'Nastya', age: 23}
-      ]},
-    {title: 'dog 2', Name: 'Piska', comments:[
-        {owner: 'Egor', age: 24},
-        {owner: 'Masha', age: 23}
-      ]}
-  ]
+  then: Date = new Date()
 }
