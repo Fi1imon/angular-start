e705682a8a83912d16207ad077ddd2aac6edb280
Fi1imon <angelo4ekfilippof@gmail.com>
2022-02-08 21:57:37 +0300

    Передача параметров

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Header {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  headers: Header[] = [
+    {title: 'Eaters of burgers', text: 'Burgers eaters are fat and ill people'},
+    {title: 'Eaters of sushi', text: 'Nya KawaII', id: 228}
+  ]
 }
