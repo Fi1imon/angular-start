7251dc68320536733e080a37f14a9b45143f16ea
Fi1imon <angelo4ekfilippof@gmail.com>
2022-02-17 21:25:07 +0300

    change detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Header {
   title: string
@@ -12,14 +12,26 @@ export interface Header {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   headers: Header[] = [
     {title: 'Eaters of burgers', text: 'Burgers eaters are fat and ill people', id: 1},
-    {title: 'Eaters of sushi', text: 'Nya KawaII', id: 228}
+    // {title: 'Eaters of sushi', text: 'Nya KawaII', id: 228}
   ]
+
+  ngOnInit() {
+    setTimeout( () => {
+      console.log('Timeout')
+      this.headers[0] = {
+        title: 'Changed',
+        text:'Be happy',
+        id: 228
+      }
+    }, 5000)
+  }
+
   updatePosts (post: Header) {
     this.headers.unshift(post)
-    console.log('Post', post)
+    // console.log('Post', post)
   }
 
   deletePost(id: number) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,19 @@
 import {
   AfterContentChecked,
-  AfterContentInit, AfterViewChecked, AfterViewInit,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ContentChild,
   DoCheck,
-  ElementRef, EventEmitter,
+  ElementRef,
+  EventEmitter,
   Input,
-  OnChanges, OnDestroy,
-  OnInit, Output,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
   SimpleChanges
 } from '@angular/core';
 import {Header} from "../app.component";
@@ -15,7 +21,8 @@ import {Header} from "../app.component";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements
   OnInit,
